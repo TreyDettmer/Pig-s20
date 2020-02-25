aa17e317522833119c2b5fb53252622b0fa9121f
vickersk <kaiv14@comcast.net>
2020-02-24 19:36:49 -0800

    Checkpoint 2

---
+++ app/src/main/java/edu/up/cs301/pig/PigHoldAction.java
@@ -0,0 +1,12 @@
+package edu.up.cs301.pig;
+
+import edu.up.cs301.game.GameFramework.GamePlayer;
+import edu.up.cs301.game.GameFramework.actionMessage.GameAction;
+
+public class PigHoldAction extends GameAction {
+
+    public PigHoldAction(GamePlayer gamePlayer){
+        super(gamePlayer);
+    }
+
+}
